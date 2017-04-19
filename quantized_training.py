ab7a481af60f91036596392d95154169785722c2
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-19 13:34:51 +0100

    weights

--- quantized_training.py
+++ quantized_training.py
@@ -83,6 +83,8 @@ def initialize_variables(exist, parent_dir, NUMBER_OF_CLUSTER, pretrain):
         weights_index[key] = tmp
     print('in initial vairables')
     print(np.shape(weights_index['cov1']))
+    print((weights_index['cov1'][0]))
+    sys.exit()
     # weights_index = {
     #     'cov1': cluster_index['cov1'],
     #     'cov2': cluster_index['cov2'],
