83582211c79f6d3be3262ed7ad664ea8b01105f4
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-20 00:15:48 +0100

    weights

--- run.py
+++ run.py
@@ -9,6 +9,13 @@ import Clustering_weights_cifar10
 # os.system('python training_v3.py -p4')
 # os.system('python training_v3.py -p5')
 
+def dump_to_txt_files(pt_acc_list, acc_list):
+    with open("ptacc_cifar_quantize_han.txt", "w") as f:
+        for item in pt_acc_list:
+            f.write("%s\n"%item)
+    with open("acc_cifar_quantize_han.txt", "w") as f:
+        for item in acc_list:
+            f.write("%s\n"%item)
 acc_list = []
 pt_acc_list = []
 count = 0
@@ -54,16 +61,8 @@ while (count < len(cluster)):
     train_acc = quantized_training.main(param)
     pt_acc_list.append(pre_train_acc)
     acc_list.append(train_acc)
-    dump_to_txt_files(pt_acc_list, acc_list)
     count = count + 1
+dump_to_txt_files(pt_acc_list, acc_list)
 print('accuracy summary: {}'.format(pt_acc_list))
 print('accuracy summary: {}'.format(acc_list))
 # acc_list = [0.82349998, 0.8233, 0.82319999, 0.81870002, 0.82050002, 0.80400002, 0.74940002, 0.66060001, 0.5011]
-
-def dump_to_txt_files(pt_acc_list, acc_list):
-    with open("ptacc_cifar_quantize_han.txt", "w") as f:
-        for item in pt_acc_list:
-            f.write("%s\n"%item)
-    with open("acc_cifar_quantize_han.txt", "w") as f:
-        for item in acc_list:
-            f.write("%s\n"%item)
