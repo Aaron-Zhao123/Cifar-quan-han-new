2a4a1039a069f0a3943f35a6b8f7b622d24c05f6
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-19 13:49:46 +0100

    weights

--- quantized_training.py
+++ quantized_training.py
@@ -115,7 +115,8 @@ def compute_weights(weights_index, centroids_var, Number_of_cluster):
     print('in compute weights')
     print(np.shape(weights_index['cov1']))
     for key in keys:
-        weights[key] = tf.matmul(tf.cast(weights_index[key], tf.float32), tf.cast(centroids_var[key], tf.float32),a_is_sparse=True)
+        ndim = len(np.shape(weights[key]))
+        weights[key] = tf.tensordot(tf.cast(weights_index[key], tf.float32), tf.cast(centroids_var[key], tf.float32),axes=[[ndim],[0]])
         # weights[key] = tf.to_float(tf.equal(weights_index[key], 1)) * centroids_var[key][0]
         # print(weights_index[key])
         # for i in range(1, Number_of_cluster + 1):
